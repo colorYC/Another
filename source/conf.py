cec794ced284e7407ff9edb8f46c32965dc8cfeb
于彩彩 <color.yu@juphoon.com.cn>
2019-01-25 17:55:48 +0800

    111

--- source/conf.py
+++ source/conf.py
@@ -167,12 +167,69 @@ htmlhelp_basename = 'CloudDocdoc'
 #     u'colorY', 'manual'),
 #]
 
-import os
+#import os
+#
+#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#
+#if on_rtd:
+#    latex_elements = {
+## The paper size ('letterpaper' or 'a4paper').
+##'papersize': 'letterpaper',
+#
+## The font size ('10pt', '11pt' or '12pt').
+##'pointsize': '10pt',
+#
+## Additional stuff for the LaTeX preamble.
+##latex_preamble = '\usepackage{tex-live-zh-cn}'
+#
+#'papersize': 'a4paper',
+#'preamble': r'''
+#    \hypersetup{unicode=true}
+#    \usepackage{CJKutf8}
+#    \DeclareUnicodeCharacter{00A0}{\\nobreakspace}
+#    \DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
+#    \DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
+#    \DeclareUnicodeCharacter{2713}{x}
+#    \DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
+#    \DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
+#    \DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
+#    \DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
+#    \DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
+#    \usepackage[UTF8, scheme = plain]{ctex}
+#    \\begin{CJK}{UTF8}{gbsn}
+#    \AtEndDocument{\end{CJK}}
+#    '''
+#    }
+#else:
+#    latex_elements = {
+#    # 'papersize' : 'a4paper',
+#    # 'utf8extra' : '',
+#    # 'inputenc'  : '',
+#    # 'babel'     : r'''\usepackage[tex-live-zh-cn]{babel}''',
+#    # 'preamble' : r'''
+#    # \usepackage{ctex}
+#    # ''',
+#    'papersize': 'a4paper',
+#    'preamble': r'''
+#        \hypersetup{unicode=true}
+#        \usepackage{CJKutf8}
+#        \DeclareUnicodeCharacter{00A0}{\nobreakspace}
+#        \DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
+#        \DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
+#        \DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
+#        \DeclareUnicodeCharacter{2713}{x}
+#        \DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
+#        \DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
+#        \DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
+#        \DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
+#        \DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
+#        \begin{CJK}{UTF8}{gbsn}
+#        \AtEndDocument{\end{CJK}}
+#
+#        ''',
+#}
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if on_rtd:
-    latex_elements = {
+latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
 
@@ -180,14 +237,13 @@ if on_rtd:
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#latex_preamble = '\usepackage{tex-live-zh-cn}'
-
-'papersize': 'a4paper',
+#'preamble': '',
 'preamble': r'''
     \hypersetup{unicode=true}
     \usepackage{CJKutf8}
-    \DeclareUnicodeCharacter{00A0}{\\nobreakspace}
+    \DeclareUnicodeCharacter{00A0}{\nobreakspace}
     \DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
+    \DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
     \DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
     \DeclareUnicodeCharacter{2713}{x}
     \DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
@@ -195,38 +251,9 @@ if on_rtd:
     \DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
     \DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
     \DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
-    \usepackage[UTF8, scheme = plain]{ctex}
-    \\begin{CJK}{UTF8}{gbsn}
+    \begin{CJK}{UTF8}{gbsn}
     \AtEndDocument{\end{CJK}}
-    '''
-    }
-else:
-    latex_elements = {
-    # 'papersize' : 'a4paper',
-    # 'utf8extra' : '',
-    # 'inputenc'  : '',
-    # 'babel'     : r'''\usepackage[tex-live-zh-cn]{babel}''',
-    # 'preamble' : r'''
-    # \usepackage{ctex}
-    # ''',
-    'papersize': 'a4paper',
-    'preamble': r'''
-        \hypersetup{unicode=true}
-        \usepackage{CJKutf8}
-        \DeclareUnicodeCharacter{00A0}{\nobreakspace}
-        \DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
-        \DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
-        \DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
-        \DeclareUnicodeCharacter{2713}{x}
-        \DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
-        \DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
-        \DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
-        \DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
-        \DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
-        \begin{CJK}{UTF8}{gbsn}
-        \AtEndDocument{\end{CJK}}
-        
-        ''',
+    ''',
 }
 
 #f = open('latex-styling.tex', 'r+');
